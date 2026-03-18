24ac9ff07293d5c2d9a54940c535b2b2ed154053
IrisLaparis <iris_sunbreeze@foxmail.com>
2026-03-18 19:28:04 +0800

    Update app.py

--- app.py
+++ app.py
@@ -7,6 +7,10 @@ WECOM_WEBHOOK = "https://qyapi.weixin.qq.com/cgi-bin/webhook/send?key=42717010-0
 app = Flask(__name__)
 CORS(app)
 
+@app.route("/ping", methods=["GET"])
+def ping():
+    return "ok", 200
+
 def send_to_wecom(text):
     payload = {"msgtype": "markdown", "markdown": {"content": text}}
     requests.post(WECOM_WEBHOOK, json=payload)
